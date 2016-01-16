e090eaf5e7d0ba44ac818ca37ff8fa3a17892702
zharley18 <zackharley@gmail.com>
2016-01-16 09:12:22 -0500

    Add basic animation

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function drawerOpen() {
+    $('#overlay').css('visibility', 'visible');
+    $('#drawer').css('visibility', 'visible');
+}
+function drawerClose() {
+    $('#overlay').css('visibility', 'hidden');
+    $('#drawer').css('visibility', 'hidden');
+}
