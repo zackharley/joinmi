dec3ebcef6732f2646d684b6960914d89eae2711
zharley18 <zackharley@gmail.com>
2016-01-17 03:35:39 -0500

    Update FB splash page

---
+++ js/landing.js
@@ -0,0 +1,5 @@
+$('document').ready(function(){
+    $('#fb').click(function(){
+        $('#splash').slideDown("slow").delay(1000).slideUp("slow");
+    });
+});
--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-
-function drawerClose() {
-    $('#overlay').css('visibility', 'hidden');
-    $('#drawer').css('visibility', 'hidden');
-}
-
 $('document').ready(function () {
     var check = 0;
     $('#notification').click(function () {
-        if(check === 0){
+        if (check === 0) {
             $("#search").slideDown("slow");
             check = 1;
-        } else{
+        } else {
             $("#search").slideUp("slow");
             check = 0;
-        }        
+        }
     });
-    
-    $('#hamburger').click(function(){
-        $('#overlay').css('visibility', 'visible');
+
+    $('#hamburger').click(function () {
+        $('#overlay').animate({
+            left: '0px'
+        }, 1000);
         $('#drawer').animate({
             left: '0px'
+        }, 1000);
+    });
+    $("#overlay").click(function () {
+        $(this).animate({
+            left: '-1080px'
+        }, 1000);
+        $('#drawer').animate({
+            left: '-900px'
         }, 1000);
     });
 });
